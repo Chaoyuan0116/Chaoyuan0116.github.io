a69203758ab15a443c0da44665367307cc18907b
chy <969610728@qq.com>
2023-05-11 23:00:13 +0800

    newnewnew

--- -imagine/js/imaginescript.js
+++ -imagine/js/imaginescript.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll",function(){
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var alpha = 1 - scrollTop / 350; // 根据页面滚动距离计算透明度
+    var alpha = 1 - scrollTop / 500; // 根据页面滚动距离计算透明度
 
     if (alpha < 0) alpha = 0; // 透明度最小为0
     if (alpha > 1) alpha = 1; // 透明度最大为1
